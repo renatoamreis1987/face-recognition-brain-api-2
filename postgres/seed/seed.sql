f0be10d3b3fe8fe5f148ded9ad56cf537d1eeb98
Renato Reis <email@renatoamreis.com>
2019-06-27 10:45:30 +0200

    small change on seed user

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) VALUES ('Renato', 'renatoamreis@gmail.com', 5, '2018-06-26');
+INSERT into users (name, email, entries, joined) VALUES ('Renato', 'renatoamreis@gmail.com', 999, '2018-06-26');
 INSERT into login (hash, email) VALUES ('$2a$10$Qzp3n9UrwX7ULePZphbhvug.jvvx61OKNJrgBBS9FXDYSbVy./L3W', 'renatoamreis@gmail.com');
 
 COMMIT;
